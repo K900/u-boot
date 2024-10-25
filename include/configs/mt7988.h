044d6e154092782a27e1c204618c1ee7b4472289
K900 <me@0upti.me>
2024-10-25 15:57:46 +0300

    arm: dts: mt7988: add bpi-r4 DT + config

--- include/configs/mt7988.h
+++ include/configs/mt7988.h
@@ -11,4 +11,12 @@
 
 #define CFG_MAX_MEM_MAPPED		0xC0000000
 
+// Start at 1.25G, reserve 256M for the kernel, and 16M for the device tree. 
+// We have RAM, it's fine (probably).
+#define CFG_EXTRA_ENV_SETTINGS \
+	"kernel_addr_r=0x50000000\0" \
+    "fdt_addr_r=0x60000000\0" \
+    "ramdisk_addr_r=0x61000000\0" \
+    "fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0"
+
 #endif
